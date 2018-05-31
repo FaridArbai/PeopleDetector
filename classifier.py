0c5c06225b8f1b035ec32b4e583fb475c1bc1707
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-06-01 00:03:15 +0200

    Updated classifier template

--- classifier.py
+++ classifier.py
@@ -1,3 +1,16 @@
-class Clasifier:
-  def __init__:
-    #info
+import cv2
+import numpy as np
+from sklearn import svm
+
+# This is the class that represents the best
+# person classifier according to my experimental
+# results
+
+class Classifier:
+  def __init__(self):
+    # Initialize constants
+    # Choose a classifi¡ying method
+  
+  def classify(img):
+    # Core function to classify pedestrians
+    
