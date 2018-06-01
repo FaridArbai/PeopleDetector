285adfd6b53f2f980625512ebce94c740cb6ce90
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-06-01 23:51:40 +0200

    Including paths to stored feature-sets

--- main.py
+++ main.py
@@ -1,48 +1,171 @@
-import numpy as np;
-import cv2;
-import sklearn as sk;
+import numpy as np
+import cv2
+import os
+from sklearn import svm, linear_model, metrics
+from database_creation import *
+from features_extraction import *
+import human_detection as HumanDetection
+from sklearn.externals import joblib
 
-class Detector:
-  #Fill the following parameters according
-  #to previous research
-  
-  _SVM_THRESHOLD = ;
-  _BLOCK_SIZE = ;
-  _ANGLE_BINS = ;
-  
-  
-  @staticmethod
-  def train():
-    #train the SVM to get the (N-1) dimension
-    #boundary function which separates the
-    #decision regions of the binary classes
-    #which decide if there's a person or not
-    #within the analised ROI
-    
-  @staticmethod
-  def detect():
-    #Project the feature vector into the SVM
-    #space in order to decide wether the
-    #current ROI encapsulates a person or nor
-    
-    
-   
-   
-if (__name__=="__main__"):
-  Detector.detect();
+ORIG_NEG_PATH = "database/INRIAPerson/Train/neg";
 
+TRAINING_POS_PATH = "database/INRIAPerson/96X160H96/Train/pos";
+TRAINING_NEG_PATH = "database/INRIAPerson/96X160H96/Train/neg";
 
+TESTING_POS_PATH = "database/INRIAPerson/70X134H96/Test/pos";
+TESTING_NEG_PATH = "database/INRIAPerson/70X134H96/Test/neg";
 
+TRAINING_FEATURES_PATH = "extracted_features/training_features.npy";
+TRAINING_LABELS_PATH		= "extracted_features/training_labels.npy";
 
+TESTING_FEATURES_PATH 	= 	"extracted_features/testing_features.npy";
+TESTING_LABELS_PATH		=	"extracted_features/testing_labels.npy";
 
+CLASSIFIER_PATH = "classifiers/linear.pkl";
 
+kx = np.asarray([[-1], [0], [1]])
+ky = np.asarray([[-1], [0], [1]])
+DERIV_KERNEL = (kx,ky);
 
+def main():
+	classifier = joblib.load(CLASSIFIER_PATH);
+	HumanDetection.detect("testing_pedestrians/person.png",classifier);
+	
+	
+	
+	'''
+		#this is to generate and save the svm
+		training_features = np.load(TRAINING_FEATURES_PATH);
+		training_labels = np.load(TRAINING_LABELS_PATH);
 
-
-
-
-
+		testing_features = np.load(TESTING_FEATURES_PATH);
+		testing_labels = np.load(TESTING_LABELS_PATH);
 
 
+		classifier = svm.LinearSVC(C=0.01);
+
+		classifier.fit(training_features, training_labels);
+
+		joblib.dump(classifier,CLASSIFIER_PATH);
+	'''
+	
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(__name__=="__main__"):
+	main();
