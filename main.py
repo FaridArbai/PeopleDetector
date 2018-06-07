1085c07dea3189147d19e5e5a4c2e701131447a8
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-06-07 23:17:55 +0200

    Added graphing functionality

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ from sklearn.externals import joblib
 import tkinter as tk
 from tkinter import filedialog
 #import h5py
+from matplotlib import pyplot as plt
+import warnings
+from matplotlib.cbook import MatplotlibDeprecationWarning
+from matplotlib.ticker import MultipleLocator
+
+warnings.simplefilter("ignore",
+							 category = MatplotlibDeprecationWarning);
+#Ignorar los warnings de matplotlib
+
 
 ORIG_NEG_PATH = "database/INRIAPerson/Train/neg";
 
@@ -18,13 +27,13 @@ TRAINING_NEG_PATH = "database/INRIAPerson/96X160H96/Train/neg_bigdata";
 TESTING_POS_PATH = "database/INRIAPerson/70X134H96/Test/pos";
 TESTING_NEG_PATH = "database/INRIAPerson/70X134H96/Test/neg";
 
-TRAINING_FEATURES_PATH = "extracted_features/training_features_bigdata_100.npy";
-TRAINING_LABELS_PATH		= "extracted_features/training_labels_bigdata_100.npy";
+TRAINING_FEATURES_PATH = "extracted_features/training_features2.npy";
+TRAINING_LABELS_PATH		= "extracted_features/training_labels2.npy";
 
 TESTING_FEATURES_PATH 	= 	"extracted_features/testing_features2.npy";
 TESTING_LABELS_PATH		=	"extracted_features/testing_labels2.npy";
 
-CLASSIFIER_PATH = "classifiers/linear3.pkl";
+CLASSIFIER_PATH = "classifiers/linear_soft.pkl";
 
 kx = np.asarray([[-1], [0], [1]])
 ky = np.asarray([[-1], [0], [1]])
@@ -36,13 +45,9 @@ def classifyVideo():
 	root = tk.Tk()
 	root.withdraw();
 	
-	#while(True):
-	#	image_path = filedialog.askopenfilename();
-	#	HumanDetection.detect(image_path,classifier);
+	video_path = filedialog.askopenfilename();
 	
-	image_path = filedialog.askopenfilename();
-	
-	cap = cv2.VideoCapture(image_path)
+	cap = cv2.VideoCapture(video_path)
 	
 	
 	count = 0
@@ -56,25 +61,6 @@ def classifyVideo():
 		if cv2.waitKey(10)&0xFF==ord('q'):
 			break
 	
-	'''
-	hog = cv2.HOGDescriptor()
-	hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector());
-	while True:
-		_, frame = cap.read()
-		found, w = hog.detectMultiScale(frame, winStride=(8, 8), padding=(32, 32), scale=1.05)
-		
-		for x1, y1, w, h in found:
-			x2 = x1 + w;
-			y2 = y1 + h;
-			cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2);
-		
-		cv2.imshow('feed', frame)
-		ch = 0xFF&cv2.waitKey(1)
-		if ch==27:
-			break
-	cv2.destroyAllWindows()
-	'''
-	
 
 
 def classifyImage():
@@ -87,6 +73,8 @@ def classifyImage():
 		image_path = filedialog.askopenfilename();
 		image = cv2.imread(image_path);
 		HumanDetection.detect(image,classifier);
+	
+	
 	
 
 def generateTrainingFeatures():
@@ -114,40 +102,113 @@ def generateClassifier():
 	features = np.load(TRAINING_FEATURES_PATH);
 	labels = np.load(TRAINING_LABELS_PATH);
 	
-	classifier = svm.LinearSVC(C=0.01);
+	#classifier = svm.LinearSVC(C=0.039062);
+	classifier = svm.LinearSVC(C=6.666);
 	classifier.fit(features,labels);
 	
 	joblib.dump(classifier, CLASSIFIER_PATH);
 
+'''
 def generateClassifier(features, labels):
 
 	classifier = svm.LinearSVC(C=0.01);
 	classifier.fit(features,labels);
 	
 	joblib.dump(classifier, CLASSIFIER_PATH);
-	
+'''
+
 def generateClassifierWithRange():
 	features_training = np.load(TRAINING_FEATURES_PATH);
 	labels_training = np.load(TRAINING_LABELS_PATH);
 	features_test = np.load(TESTING_FEATURES_PATH);
 	labels_test = np.load(TESTING_LABELS_PATH);
 	
-	C = []
+	v_c = []
 	
-	for i in range(0,7):
-		C.append(10**(-i));
+	for i in range(-12,5):
+		v_c.append(2**i);
+		
+	 
+	fpr = np.zeros((len(v_c)));
+	tnr = np.zeros((len(v_c)));
+	miss = np.zeros((len(v_c)));
+	i = 0;
 	
-	for c in C:
+	for c in v_c:
 		classifier = svm.LinearSVC(C=c);
 		classifier.fit(features_training, labels_training);
-		score = classifier.score(features_test, labels_test);
-		print(score);
+		
+		predictions = classifier.predict(features_test);
+		n_positives = np.sum(predictions==labels_test);
+		score = (n_positives/len(labels_test));
+		
+		
+		
+		confusion = metrics.confusion_matrix(labels_test, predictions);
+		
+		print(confusion)
+		print(("%f : %f")%(c, score*100));
+		
+		fpr[i] = (confusion[0][1]/np.sum(confusion[0]));
+		tnr[i] = (confusion[1][0]/np.sum(confusion[1]));
+		miss[i] = 1-score;
+		i+=1;
+	
+	plt.figure(1);
+	plt.semilogx(v_c, 10*np.log10(fpr), basex=2, color="r", marker="s", linestyle="--", markerfacecolor="none",
+				  linewidth=0.75, label="Falsos positivos");
+	plt.hold(True);
+	
+	plt.semilogx(v_c, 10*np.log10(tnr), basex=2, color="b", marker="^", linestyle="--", markerfacecolor="none",
+				  linewidth=0.75, label="Verdaderos negativos");
+	
+	plt.xlabel("Factor de penalizacion (C)");
+	plt.ylabel("Error [dB]");
+	plt.title("Tasas de errores frente a regularización");
+	plt.grid(True, linestyle="--", linewidth=0.5);
+	
+	plt.legend();
+	
+	
+	plt.figure(2);
+	plt.semilogx(v_c, 10*np.log(miss), basex=2, color="k", marker="o", linestyle="--", markerfacecolor="none",
+				  linewidth=0.75, label="Tasa de error");
+	
+	plt.xlabel("Factor de penalizacion (C)");
+	plt.ylabel("Error [dB]");
+	plt.title("Tasa de error frente a regularización");
+	plt.grid(True, linestyle="--", linewidth=0.5);
+	
+	plt.show();
+	
 	
 
 def test():
 	features = np.load(TESTING_FEATURES_PATH);
 	labels = np.load(TESTING_LABELS_PATH);
 	classifier = joblib.load(CLASSIFIER_PATH);
+	
+	predictions = classifier.predict(features);
+	n_positives = np.sum(predictions==labels);
+	accuracy = (n_positives/len(labels))*100;
+	
+	confusion_matrix = metrics.confusion_matrix(labels,predictions);
+	
+	print("Accuracy : %f"%(accuracy));
+	print("Confussion matrix : ");
+	print(confusion_matrix);
+
+def test2():
+	features = np.load(TESTING_FEATURES_PATH);
+	labels = np.load(TESTING_LABELS_PATH);
+	
+	hog = cv2.HOGDescriptor()
+	parameters = cv2.HOGDescriptor_getDefaultPeopleDetector();
+	
+	classifier = svm.LinearSVC(C=parameters[-1]);
+	classifier.coef_ = np.transpose(parameters[0:3780]);
+	
+	#classifier.set_params(coef_=np.transpose(parameters[0:3780]));
 	
 	predictions = classifier.predict(features);
 	
@@ -162,32 +223,18 @@ def main():
 	#generateTestingFeatures();
 	#generateTrainingFeatures();
 	#test();
-	#generateClassifierWithRange()
+	generateClassifierWithRange()
 	
-	
-	classifyImage();
+	#classifyImage();
 	#classifyVideo();
 	
 	#generateTrainingFeatures();
 	#generateClassifier();
 	#test();
 	
-	'''
-	features, labels = obtainDataFeatures(TRAINING_POS_PATH,
-													  TRAINING_NEG_PATH);
+	#test2();
 	
-	classifier = svm.LinearSVC(C=0.01);
-	classifier.fit(features,labels);
 	
-	joblib.dump(classifier, "classifiers/linear_bigdata_50.pkl");
-	
-	test();
-													  
-	h5_file = h5py.File("extracted_features/training_features_bigdata_50.h5", 'w');
-	h5_file.create_dataset("features", data=features);
-	
-	h5_file.close();
-	'''
 
 
 
